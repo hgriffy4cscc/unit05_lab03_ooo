fbc69de4c06ca1d238976a83bd40c869fd636216
Henry <hgriffy@student.cscc.edu>
2026-07-05 13:19:22 -0400

    initial commit: files + texts in place

---
+++ Lab10_griffy-1.py
@@ -0,0 +1,13 @@
+"""
+For this assignment, you will create a single "Word Count" project. Your program will present a menu of 4 predefined files (Files are attached below). The user will select a file, and your program will analyze it. Your core logic must be encapsulated within a class.
+
+Program Name: Lab10_griffy-1.py
+Author: Henry Griffy
+The purpose of the program: Generates word counts for a selected file
+Starter Code:
+    Course Texts:
+        Python Crash Course 3rd ed Chapter 10
+"""
+
+from pathlib import Path
+
