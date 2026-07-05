7e086d464cde178ba695351ea826d947600b37c9
Henry <hgriffy@student.cscc.edu>
2026-07-05 13:40:15 -0400

    basic objjects created and initialized

--- Lab10_griffy-1.py
+++ Lab10_griffy-1.py
@@ -11,3 +11,34 @@ Starter Code:
 
 from pathlib import Path
 
+class MyLib():
+    texts: list[MyText] = []
+
+    def __init__(self):
+        pass
+
+    def add_new_text(self, *new_text: MyText ):
+        self.texts.append(new_text)
+    
+class MyText():
+    text_title: str = ""
+    text_path: Path = None
+
+    def __init__(self, t_title, t_path):
+        self.text_title = t_title
+        self.text_path = Path(t_path)
+
+
+if __name__ == "__main__":
+    texts_in = [
+        ("Princess of Mars","texts/princess_mars.txt"),
+        ("Tarzan","texts/Tarzan.txt"),
+        ("Treasure Island","texts/treasure_island.txt"),
+        ("The Count of Monte Cristo","texts/monte_cristo.txt")
+        ]
+    the_lib = MyLib()
+    for t in texts_in:
+        the_lib.add_new_text(t[0], t[1])
+    print(len(the_lib.texts))
+
+
